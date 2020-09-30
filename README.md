74d5ff8e52c005fd1816383e0380ce0d8d12d68b
Daniel Labios <labiosd@gmail.com>
2020-09-30 16:07:22 -0400

    problemo need solving-o

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # React Calculator
+
+Algorithm
+
+    Enter first operand
+    1 or more of the digit buttons
+    Append each digit to the first operand
+    First operand entry ends when an operator is pressed
+    Append each digit to display as clicked
+    Enter operator
+    stores state in both first operand and operator
+    Enables entry of second operator
+    Clears display
+    Enter second operand
+    1 or more of the digit buttons
+    Append each digit to the second operand
+    second operand entry ends when an equal button is pressed
+    Append each digit to display as clicked
+    Enter equal button
+    stores state second operand
+    Calculates result
+    Puts result on display
+    AC button
+    At any point of process clears all states
+
+No idea how to handle decimal point yet.
+
+Algorithm
+
+initial state display is set to 0
+//state of 1+2 operand and operator?
+
+-user clicks digit
+-store that digit as the first operand
+-update the display to show that digit
+(do over above as it keeps going)
+-user clicks operator (+-\*/)
+-store the operator
+//-do nothing with the first operand, leave it alone//
+//-do nothing with the display, leave it alone//
+-user clicks digit
+-store that digit as second operand
+-update display to show the result
+-user clicks operator
+-calculate the result (first+second Operand), store it as first operand
+
+-when user clicks =
+-calculate result
+-update display to show the result
+
+-user clicks AC(clear)
+-clear the first operand
+-clear the second operand
+-clear the operator
+-set display as 0
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react'
+
+export function Calculator() {
+  //declare state below [first,second] first is state name, setDisplay is a function used to change it
+  const [display, setDisplay] = useState(0)
+  //simple Defined Click Function on Clear Button
+
+  const [firstNumber, setFirstNumber] = useState(null)
+  const [secondNumber, setSecondNumber] = useState(null)
+  const [operator, setOperator] = useState(null)
+  const [result, setResult] = useState(null)
+  //const [showEquals, setShowEquals] = useState(false)
+  //////////////////////////////////////////////////////////////////////
+
+  function clickClearButton() {
+    setDisplay(0)
+    setFirstNumber(null)
+    setSecondNumber(null)
+    setOperator(null)
+    setResult(null)
+  }
+  function clickedDigit(event) {
+    if (result !== null) {
+      clickClearButton()
+      //console.log('huh')
+    }
+    if (operator === null) {
+      if (display === 0) {
+        setDisplay(event)
+        setFirstNumber(event)
+      } else {
+        setDisplay(`${display}${event}`)
+        setFirstNumber(`${display}${event}`)
+      }
+    } else {
+      if (display === 0) {
+        setDisplay(event)
+        setSecondNumber(event)
+      } else {
+        setDisplay(`${display}${event}`)
+        setSecondNumber(`${display}${event}`)
+      }
+    }
+  }
+  function clickOperator(event) {
+    setOperator(event)
+    setDisplay(0)
+  }
+
+  function clickEquals() {
+    let actualFirstNumber = parseInt(firstNumber, 10)
+    let actualSecondNumber = parseInt(secondNumber, 10)
+
+    if (operator === '+') {
+      setDisplay(actualFirstNumber + actualSecondNumber)
+      setResult(actualFirstNumber + actualSecondNumber)
+    }
+    if (operator === '-') {
+      setDisplay(actualFirstNumber - actualSecondNumber)
+      setResult(actualFirstNumber - actualSecondNumber)
+    }
+    if (operator === '/') {
+      setDisplay(actualFirstNumber / actualSecondNumber)
+      setResult(actualFirstNumber / actualSecondNumber)
+    }
+    if (operator === '*') {
+      setDisplay(actualFirstNumber * actualSecondNumber)
+      setResult(actualFirstNumber * actualSecondNumber)
+    } else {
+      return
+    }
+    //setShowEquals(true)
+  }
+
+  //////////////////////////////////////////////////////////////////////
+
+  return (
+    <main>
+      <div className="calculator">
+        <div className="display">{display}</div>
+        <div className="buttons">
+          <button className="button fn" onClick={clickClearButton}>
+            AC
+          </button>
+          <button className="button fn">&#177;</button>
+          <button className="button fn">&#37;</button>
+          <button className="button op" onClick={() => clickOperator('/')}>
+            &#247;
+          </button>
+          <button className="button" onClick={() => clickedDigit(7)}>
+            7
+          </button>
+          <button className="button" onClick={() => clickedDigit(8)}>
+            8
+          </button>
+          <button className="button" onClick={() => clickedDigit(9)}>
+            9
+          </button>
+          <button className="button op" onClick={() => clickOperator('*')}>
+            &#215;
+          </button>
+          <button className="button" onClick={() => clickedDigit(4)}>
+            4
+          </button>
+          <button className="button" onClick={() => clickedDigit(5)}>
+            5
+          </button>
+          <button className="button" onClick={() => clickedDigit(6)}>
+            6
+          </button>
+          <button className="button op" onClick={() => clickOperator('-')}>
+            &#8722;
+          </button>
+          <button className="button" onClick={() => clickedDigit(1)}>
+            1
+          </button>
+          <button className="button" onClick={() => clickedDigit(2)}>
+            2
+          </button>
+          <button className="button" onClick={() => clickedDigit(3)}>
+            3
+          </button>
+          <button className="button op" onClick={() => clickOperator('+')}>
+            &#43;
+          </button>
+          <button className="button x2" onClick={() => clickedDigit(0)}>
+            0
+          </button>
+          <button className="button">.</button>
+          <button className="button op" onClick={clickEquals}>
+            &#61;
+          </button>
+        </div>
+      </div>
+    </main>
+  )
+}
